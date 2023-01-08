4e77877838130e24aca1c46f64578dadfc59d315
Akashnayakala <111497694+Akashnayakala@users.noreply.github.com>
2023-01-08 19:14:02 +0530

    a=int(input()) l=list(map(int,input().split())) b,c=map(int,input().split()) c2=[] c1=0 m1=max(b,c) m2=min(b,c) for i in l:     if i>=m2 and i<=m1:         c2.append(i)         c1=1 if c1==0:     print("-1") else:     print(*c2)

---
+++ elements_in_between_A_and_B.py
@@ -0,0 +1,15 @@
+a=int(input())
+l=list(map(int,input().split()))
+b,c=map(int,input().split())
+c2=[]
+c1=0
+m1=max(b,c)
+m2=min(b,c)
+for i in l:
+    if i>=m2 and i<=m1:
+        c2.append(i)
+        c1=1
+if c1==0:
+    print("-1")
+else:
+    print(*c2)
